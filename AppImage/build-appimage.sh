367b40248d8d0feb13ee9b2115087d730f77393b
Михаил Павлов <pavlovma007@gmail.com>
2024-02-13 22:31:09 +0300

    i build it , release . not well tested

--- AppImage/build-appimage.sh
+++ AppImage/build-appimage.sh
@@ -10,33 +10,44 @@ fi
 set +e
 APP=Korni
 patharch=x86_64
-#package="brave-browser_1.33.100_amd64.deb"
-#URL="https://github.com/brave/brave-browser/releases/"
-#if  [ ! -f $package ]; then
-    #wget -c $URL$package
-#else
-    #echo "$package file exists"
-#fi
 
 mkdir -p ./$APP.AppDir
 rm -rf ./$APP.AppDir/*
 cd $APP.AppDir/
-#find ../ -name *.deb -exec dpkg -x \{\} . \;
 cp /usr/local/bin/korni3 . 
-cp ../icon.jpg .
+cp ../icon.png .
 ldd `which korni3 `| awk '{print $3}' | xargs -I {} sh -c 'cp {} ./'
 
+sed -i -e 's#/usr#././#g' korni3
+sed -i -e 's#/usr#././#g' libc.so.6
+sed -i -e 's#/usr#././#g' libcrypto.so.1.1
+sed -i -e 's#/usr#././#g' libdl.so.2
+sed -i -e 's#/usr#././#g' libgcc_s.so.1
+sed -i -e 's#/usr#././#g' libm.so.6
+sed -i -e 's#/usr#././#g' libpthread.so.0
+sed -i -e 's#/usr#././#g' libsqlite3.so.0
+sed -i -e 's#/usr#././#g' libstdc++.so.6
+#
+sed -i -e 's#/lib#././#g' korni3
+sed -i -e 's#/lib#././#g' libc.so.6
+sed -i -e 's#/lib#././#g' libcrypto.so.1.1
+sed -i -e 's#/lib#././#g' libdl.so.2
+sed -i -e 's#/lib#././#g' libgcc_s.so.1
+sed -i -e 's#/lib#././#g' libm.so.6
+sed -i -e 's#/lib#././#g' libpthread.so.0
+sed -i -e 's#/lib#././#g' libsqlite3.so.0
+sed -i -e 's#/lib#././#g' libstdc++.so.6
+strings korni3 | grep /usr
 
-#cp opt/brave.com/brave/product_logo_48.png ./Brave.png
 
 cat > $APP.desktop <<EOF
 [Desktop Entry]
 Type=Application
 Name=korni3
 Exec=korni3
-Icon=icon.png
+Icon=icon
 StartupNotify=false
-Categories=Network;Database;Office;
+Categories=Utility;
 Comment='платформа личного цифрового суверенитета'
 EOF
 #MimeType=text/html;text/xml;application/xhtml_xml;image/webp;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;
@@ -52,20 +63,24 @@ else
 fi
 chmod a+x AppRun
 
+mkdir -p ./usr # NEED that ! WhY ? - i dont know. MUST have it . fuck
 
 # generate_type2_appimage
 cd ..
 pwd 
 VERSION=""
+# to korni3--x86_64.AppImage
 #'appimagetool-x86_64.AppImage' --appimage-extract
-#VERSION=$VERSION './appimagetool-x86_64.AppImage' -v -n ./$APP.AppDir/
+ARCH='x86_64' VERSION=$VERSION './appimagetool-x86_64.AppImage' -v -n ./$APP.AppDir/
 #ARCH='x86_64' VERSION=$VERSION './appimagetool-x86_64.AppImage'  ./$APP.AppDir/
-mksquashfs Korni.AppDir/ Korni3.squashfs -root-owned -noappend
-cat Korni.AppDir/korni3  > korni3.AppImage
-cat Korni3.squashfs >> korni3.AppImage
-chmod a+x korni3.AppImage
-rm Korni3.squashfs
 
+#mksquashfs Korni.AppDir/ Korni3.squashfs -root-owned -noappend
+#cat Korni.AppDir/korni3  > korni3.AppImage
+#cat Korni3.squashfs >> korni3.AppImage
+#chmod a+x korni3.AppImage
+#rm Korni3.squashfs
+
+## CLEAR TODO 
 #if [ -d "./$APP.AppDir/" ] ; then
 #  rm -rf ./$APP.AppDir
 #fi
